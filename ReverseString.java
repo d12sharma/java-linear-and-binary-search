e9d07fa065089419c12281dbf284141275af9a11
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-25 14:50:32 +0530

    Dhruv Sharma Add:reverse string using string builder

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        StringBuilder string1 = new StringBuilder();
+        string1.append("Hello");
+        string1.reverse();
+        string1.toString();
+        System.out.println(string1);
+
+    }
+}
